87c1e6ddb4c1ddf3cf5ff28970ff9b5e2e392c5a
masayuki038 <masayuki038@gmail.com>
2014-02-22 16:25:21 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chienowa::Application.config.secret_key_base = '753e74813d5bee32faa327850a474be74b6d8e33b35aa13bc720fdeffc78aff6a913d23e6ca7aeb3476b9c64910b911555d069908713faf4c025932f4033b1bf'
